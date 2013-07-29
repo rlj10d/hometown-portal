80af4b4520db93d40325cdbbb04982a83dc59bf6
rlj10d <rlj10d@my.fsu.edu>
2013-07-29 01:12:08 -0400

    completed education and employment sections

--- src/com/example/hometownportal/EducationActivity.java
+++ src/com/example/hometownportal/EducationActivity.java
@@ -13,6 +13,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 import android.util.Log;
 
 public class EducationActivity extends Activity {
@@ -24,8 +25,54 @@ public class EducationActivity extends Activity {
 	private static final int ID_B_PRIMARY_SCHOOLS	= R.id.bPrimarySchools;
 	private static final int ID_B_SECONDARY_SCHOOLS	= R.id.bSecondarySchools;
 	private static final int ID_B_COLLEGES			= R.id.bColleges;
-	private static final int ID_B_UNIVERSITIES		= R.id.bUniversities;
 	
+	private static final String S_PRIMARY_SCHOOLS =
+			"Primary Schools in Panama City\n\n" +
+			"Bay Haven Charter Academy\n" 	+	
+			"Callaway Elementary School\n" 	+	
+			"Cedar Grove Elementary School\n" +	
+			"Covenant Christian School\n" +
+			"Good Shepherd Lutheran School\n" +
+			"Hiland Park Elementary School\n" +	
+			"Holy Nativity Episcopal School\n" +
+			"Hutchison Beach Elementary School\n" +	
+			"Kaleidoscope School Of Discovery\n" +
+			"Lucille Moore Elementary School\n" +		
+			"Merriam Cherry Street Elementary\n" +	
+			"Millville Elementary School\n" +
+			"Northside Elementary School\n" +
+			"Oakland Terrace School\n" + 
+			"Oscar Patterson Elementary Magnet\n" +
+			"Panama City Advanced School\n" +
+			"Panama City Christian School\n" +
+			"Parker Elementary School\n" +
+			"Springfield Elementary School\n" +
+			"St. Andrew School\n" +
+			"St. John Catholic School\n" +
+			"Tommy Smith Elementary School\n";
+	
+	private static final String S_SECONDARY_SCHOOLS =
+			"Secondary Schools in Panama City\n\n"	+
+			"A. D. Harris High School\n"	+
+			"Bay County Sheriff's Boot Camp\n"	+
+			"Bay High School\n"	+
+			"Chautauqua Charter School\n"	+
+			"Covenant Christian School\n"	+
+			"Fellowship Christian Academy\n"	+
+			"Haney Technical High School\n"	+
+			"Panama City Advanced School\n"	+
+			"Rutherford High School\n";
+	
+	private static final String S_COLLEGES_UNIV =
+			"Colleges & Universities in Panama City\n\n" +
+			"Gulf Coast State College\n" +
+			"Florida State University\n" +
+			"Embry-Riddle Aeronautical University\n";
+	
+	//
+	// Private attributes
+	//
+	private EditText _editText1;
 	
 	//
 	// Public methods
@@ -46,19 +93,19 @@ public class EducationActivity extends Activity {
 		{
 			case ID_B_PRIMARY_SCHOOLS:
 				Log.w("EducationActivity", "Button Click: Primary Schools" );
+				this._editText1.setText(S_PRIMARY_SCHOOLS);
 				break;
 				
 			case ID_B_SECONDARY_SCHOOLS:
 				Log.w("EducationActivity", "Button Click: Secondary Schools" );
+				this._editText1.setText(S_SECONDARY_SCHOOLS);
 				break;
 				
 			case ID_B_COLLEGES:
 				Log.w("EducationActivity", "Button Click: Colleges" );
+				this._editText1.setText(S_COLLEGES_UNIV);
 				break;
 				
-			case ID_B_UNIVERSITIES:
-				Log.w("EducationActivity", "Button Click: Universities" );
-				break;
 		}
 	}
 	
@@ -71,6 +118,8 @@ public class EducationActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.education);
+		
+		this._editText1 = (EditText) findViewById(R.id.editText1);
 	}
 
 	
--- src/com/example/hometownportal/EmploymentActivity.java
+++ src/com/example/hometownportal/EmploymentActivity.java
@@ -13,6 +13,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.util.Log;
 
 public class EmploymentActivity extends Activity {
@@ -24,6 +26,10 @@ public class EmploymentActivity extends Activity {
 	private static final int ID_B_JOB_LISTINGS		= R.id.bJobListings;
 	private static final int ID_B_TIPS_AND_TRICKS	= R.id.bTipsAndTricks;
 	
+	//
+	// Private attributes
+	//
+	private WebView _myWebView;
 	
 	//
 	// Public methods
@@ -40,14 +46,19 @@ public class EmploymentActivity extends Activity {
 	//-----------------------------------------------------------------------
 	public void onButtonClick(View view)
 	{
+		WebSettings webSettings = this._myWebView.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+		
 		switch( view.getId() )
-		{
+		{	
 			case ID_B_JOB_LISTINGS:
 				Log.w("EmploymentActivity", "Button Click: Job Listings" );
+				this._myWebView.loadUrl("http://m.monster.com/JobSearch/Search?jobtitle=&keywords=&where=32408");
 				break;
 				
 			case ID_B_TIPS_AND_TRICKS:
 				Log.w("EmploymentActivity", "Button Click: Tips and Tricks" );
+				this._myWebView.loadUrl("http://m.yahoo.com/w/legobpengine/finance/news/6-sure-ways-sabotage-job-114506127.html?.b=career-education%2F&.cf3=Jumbotron&.cf4=3&.cf5=Forbes&.cf6=%2Fcareer-education%2F&.ts=1366059661&.intl=us&.lang=en&.ysid=EBP99q36bhvLJlhSOFMvq0IG");
 				break;
 		}
 	}
@@ -61,6 +72,8 @@ public class EmploymentActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.employment);
+		
+		this._myWebView = (WebView) findViewById(R.id.webView1);
 	}
 
 	
